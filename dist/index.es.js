d5c37157ff3bb1d07973b59690d75de041b3eaec
zachyutw1117 <zachyu.tw@gmail.com>
2019-08-31 17:18:03 -0700

    update prod

--- dist/index.es.js
+++ dist/index.es.js
@@ -1,19 +1,21 @@
 import { useState, useCallback, useEffect } from 'react';
 
-const isInViewport = (el, {top: t, height: h} = el.getBoundingClientRect()) =>
-  t <= window.innerHeight && t + h >= 0;
-const useIsScrolledIntoView = ref => {
-  const [isIntoView, setIsIntoView] = useState(false);
-  const handleOnScroll = useCallback(() => {
-    setIsIntoView(isInViewport(ref.current));
-  }, []);
-  useEffect(() => {
-    window.addEventListener('scroll', handleOnScroll);
-    return () => {
-      window.removeEventListener('scroll', handleOnScroll);
-    };
-  }, [ref]);
-  return isIntoView;
+const isInViewport = (el, { top: t, height: h } = el.getBoundingClientRect()) => t <= window.innerHeight && t + h >= 0;
+const useIsScrolledIntoView = (ref, propState = true) => {
+    const [ isIntoView, setIsIntoView ] = useState(propState);
+    const handleOnScroll = useCallback(() => {
+        setIsIntoView(isInViewport(ref.current));
+    }, []);
+    useEffect(
+        () => {
+            window.addEventListener('scroll', handleOnScroll);
+            return () => {
+                window.removeEventListener('scroll', handleOnScroll);
+            };
+        },
+        [ ref ]
+    );
+    return isIntoView;
 };
 
 export default useIsScrolledIntoView;
--- dist/index.js
+++ dist/index.js
@@ -4,20 +4,22 @@ Object.defineProperty(exports, '__esModule', { value: true });
 
 var react = require('react');
 
-const isInViewport = (el, {top: t, height: h} = el.getBoundingClientRect()) =>
-  t <= window.innerHeight && t + h >= 0;
-const useIsScrolledIntoView = ref => {
-  const [isIntoView, setIsIntoView] = react.useState(false);
-  const handleOnScroll = react.useCallback(() => {
-    setIsIntoView(isInViewport(ref.current));
-  }, []);
-  react.useEffect(() => {
-    window.addEventListener('scroll', handleOnScroll);
-    return () => {
-      window.removeEventListener('scroll', handleOnScroll);
-    };
-  }, [ref]);
-  return isIntoView;
+const isInViewport = (el, { top: t, height: h } = el.getBoundingClientRect()) => t <= window.innerHeight && t + h >= 0;
+const useIsScrolledIntoView = (ref, propState = true) => {
+    const [ isIntoView, setIsIntoView ] = react.useState(propState);
+    const handleOnScroll = react.useCallback(() => {
+        setIsIntoView(isInViewport(ref.current));
+    }, []);
+    react.useEffect(
+        () => {
+            window.addEventListener('scroll', handleOnScroll);
+            return () => {
+                window.removeEventListener('scroll', handleOnScroll);
+            };
+        },
+        [ ref ]
+    );
+    return isIntoView;
 };
 
 exports.default = useIsScrolledIntoView;
